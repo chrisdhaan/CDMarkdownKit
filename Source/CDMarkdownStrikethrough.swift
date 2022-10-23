06f8451e5b91d67a8c47ef6440ac66451542bfc0
Christopher de Haan <chrisdhaan@gmail.com>
2022-10-23 11:32:02 +0200

    feat: Add strikethrough element

---
+++ Source/CDMarkdownStrikethrough.swift
@@ -0,0 +1,76 @@
+//
+//  CDMarkdownStrikethrough.swift
+//  CDMarkdownKit
+//
+//  Created by Christopher de Haan on 10/16/22.
+
+//  Copyright © 2016-2022 Christopher de Haan <contact@christopherdehaan.me>
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#if os(iOS) || os(tvOS) || os(watchOS)
+    import UIKit
+#elseif os(macOS)
+    import Cocoa
+#endif
+
+open class CDMarkdownStrikethrough: CDMarkdownCommonElement {
+
+    fileprivate static let regex = "()(~~)(.*?)(\\2)"
+
+    open var font: CDFont?
+    open var color: CDColor?
+    open var backgroundColor: CDColor?
+    open var paragraphStyle: NSParagraphStyle?
+    open var strikethroughColor: CDColor?
+    open var strikethroughStyle: NSUnderlineStyle?
+    open var underlineStyle: NSUnderlineStyle?
+    open var underlineColor: CDColor?
+
+    open var regex: String {
+        return CDMarkdownStrikethrough.regex
+    }
+
+    public init(font: CDFont? = nil,
+                color: CDColor? = nil,
+                backgroundColor: CDColor? = nil,
+                paragraphStyle: NSParagraphStyle? = nil,
+                strikethroughStyle: NSUnderlineStyle? = .single,
+                strikethroughColor: CDColor? = nil) {
+        self.font = font
+        self.color = color
+        self.backgroundColor = backgroundColor
+        self.paragraphStyle = paragraphStyle
+        self.strikethroughColor = strikethroughColor
+        self.strikethroughStyle = strikethroughStyle
+    }
+
+    public func addAttributes(_ attributedString: NSMutableAttributedString, range: NSRange) {
+        var adjustedAttributes = attributes
+
+        adjustedAttributes.addStrikethroughStyle(strikethroughStyle)
+        if let strikethroughColor = strikethroughColor {
+            adjustedAttributes.addStrikethroughColor(strikethroughColor)
+        }
+
+        attributedString.addAttributes(adjustedAttributes, range: range)
+    }
+
+}
